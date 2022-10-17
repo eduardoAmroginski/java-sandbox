9b48dd8f2c4fe4ca3a75fd18ba142ec5c285f40f
Eduardo Amroginski Santos <eduardo.amroginski@pearson.com>
2022-10-16 21:36:17 -0300

    Adicionando primeiros programas

---
+++ Java Programming - A Complete Java course to Become Java Ace/JavaMasterClass/src/FirstExercise.java
@@ -0,0 +1,9 @@
+public class FirstExercise {
+    public static void main(String[] args) {
+        System.out.println(printResult());
+    }
+
+    public static String printResult() {
+       return "Row, row, row your boat\nGently down the stream!!!";
+    }
+}
---
+++ Java Programming - A Complete Java course to Become Java Ace/JavaMasterClass/src/MyFirstJavaClass.java
@@ -0,0 +1,11 @@
+public class MyFirstJavaClass {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x + 2;
+        int z = x + y;
+
+        System.out.println("Z value is :: " + z);
+    }
+
+}
